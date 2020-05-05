de19c71e1e997a9472c0d27c655480afc2fec00e
antoine <anto.puech@gmail.com>
2020-05-05 02:10:23 +0200

    responsive done

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -13,12 +13,14 @@
 
             <div id="buttons">
                 <img src="./../assets/empreinte.png">
-                <a> Relevé de présence </a>
+                <a id="desktop"> Relevé de présence </a>
+                <a id="mobile"> Présence </a>
             </div>
 
             <div id="buttons">
                 <img src="./../assets/calendrier.png">
-                <a> Emploie du temps </a>
+                <a id="desktop"> Emploie du temps </a>
+                <a id="mobile"> EDT </a>
             </div>
 
             <div id="buttons">
@@ -33,7 +35,9 @@
 
             <div id="buttons">
                 <img src="./../assets/reglage.png">
-                <a> devinci online</a>
+                <a id="desktop"> devinci online</a>
+                <a id="mobile"> DVO </a>
+
             </div>
 
             <div id="buttons">
@@ -46,7 +50,8 @@
 
             <div id="buttons">
                 <img src="./../assets/partage.png">
-                <a>Occupation des salles</a>
+                <a id="desktop">Occupation des salles</a>
+                <a id="mobile"> Salles </a>
             </div>
 
             <div id="buttons">
@@ -62,15 +67,16 @@
                 <a href="https://google.fr">Ressources</a>                
             </div>
 
-
-        </div>
-
+        <div class="footer">
         <div class="etudiant">
             <img src="./../assets/Puech-Antoine.jpg" id="photoProfil">
             <p id="nom">Antoine Puech</p>
         </div>
         <p id="copyright"> ALV Online - 2020 © Léonard de Vinci </p>
+        </div>
+        </div>
     </div>
+    
 </template>
 
 <script>
@@ -86,7 +92,7 @@
 <style scoped>
 
 #menu {
-    width: 20%;
+    width: 21%;
     height: 100vh;
     background-color: #252C48;
     font-family: 'Montserrat';
@@ -111,7 +117,7 @@
 }
 
 .logo img {
-    width: 200px;
+    width: 75%;
     margin-top: 20px;
 }
 
@@ -123,8 +129,12 @@
 #button {
     display: flex;
     align-items: center;
-    padding: 0 30px;
+    padding: 0 10%;
     margin: 15px 0;
+}
+
+#mobile {
+    display: none;
 }
 
 #button a{
@@ -140,7 +150,7 @@
 #buttons {
     display: flex;
     align-items: center;
-    padding: 0 30px;
+    padding: 0 10%;
     height: 34px;
 }
 
@@ -160,7 +170,7 @@
     margin-bottom: 5px;
     margin-top: 25px;
     font-size: 10px;
-    padding: 0 35px;
+    padding: 0 20%;
     color: lightgrey;
     text-transform: capitalize;
 }
@@ -169,20 +179,23 @@
     margin-bottom: 5px;
     margin-top: 10px;
     font-size: 10px;
-    padding: 0 35px;
+    padding: 0 20%;
     color: lightgrey;
     text-transform: capitalize;
 }
 
 .etudiant {
     display: flex;
-    justify-content: center;
-    margin-top: 20%;
+    /* justify-content: center; */
+    /* margin-top: 20%; */
+    position: absolute;
+    bottom: 50px;
     align-items: center;
+    left: 50px;
 }
 
 .etudiant #photoProfil {
-    width: 15%;
+    width: 5%;
     border-radius: 100px;
     height: 15%;
     margin: 0 15px;
@@ -192,6 +205,8 @@
     padding: 0 5px;
     font-family: 'Montserrat';
     font-weight: bold;
+    font-size: 13px;
+    text-transform: none;
 }
 
 #copyright {
@@ -199,6 +214,197 @@
     color: #ffffff;
     opacity: 50%;
     text-align: center;
-    margin-top: 25px;
+    position: absolute;
+    bottom: 15px;
+    left: 30px;
+}
+
+/* responsive design here */
+
+@media screen and (min-width: 790px) and (max-width: 1120px) {
+    #button a{
+    font-size: 10px;
+    }
+    #buttons a{
+        font-size: 10px;
+    }
+    #copyright{
+        font-size: 9px;
+        color: #ffffff;
+        opacity: 50%;
+        text-align: center;
+        position: absolute;
+        bottom: 15px;
+        left: 14px;
+    }
+    .etudiant {
+        left: 10px;
+    }
+}
+@media screen and (min-width: 601px) and (max-width: 789px) {
+    #button a{
+    font-size: 8px;
+    }
+    #buttons a{
+        font-size: 8px;
+        line-height: 14px;
+    }
+    #copyright{
+        font-size: 6px;
+        color: #ffffff;
+        opacity: 50%;
+        text-align: center;
+        position: absolute;
+        bottom: 15px;
+        left: 16px;
+    }
+    .etudiant {
+        left: 10px;
+    }
+    .etudiant #nom {
+        font-size: 10px;
+    }
+    .etudiant #photoProfil {
+        margin: 0 10px;
+    }
+}
+@media screen and (min-width: 380px) and (max-width: 600px) {
+    #button a{
+        font-size: 7px;
+    }
+    #buttons a{
+        font-size: 6px;
+    }
+    #button img{
+        width: 10px;
+    }
+    #buttons img{
+        width: 10px;
+    }
+    #desktop {
+        display: none;
+    }
+    #mobile{
+        display: initial;
+    }
+    #buttons{
+        height: 32px;
+    }
+    #copyright{
+        font-size: 4px;
+        color: #ffffff;
+        opacity: 50%;
+        text-align: center;
+        position: absolute;
+        bottom: 15px;
+        left: 3px;
+    }
+    .etudiant {
+        left: 5px;
+    }
+    .etudiant #nom {
+        font-size: 6px;
+    }
+    .etudiant #photoProfil {
+        margin: 0 2px;
+    }
+}
+@media screen and (min-width: 340px) and (max-width: 379px) {
+    #button a{
+        font-size: 6px;
+        padding: 0 5px;
+    }
+    #buttons a{
+        font-size: 6px;
+        padding: 0 5px;
+    }
+    #button img{
+        width: 8px;
+    }
+    #buttons img{
+        width: 8px;
+    }
+    #desktop {
+        display: none;
+    }
+    #mobile{
+        display: initial;
+    }
+    #buttons{
+        height: 30px;
+    }
+    .service{
+        font-size: 8px;
+    }
+    .categorie{
+        font-size: 8px
+    }
+    #copyright{
+        font-size: 3.3px;
+        color: #ffffff;
+        opacity: 50%;
+        text-align: center;
+        position: absolute;
+        bottom: 15px;
+        left: 5px;
+    }
+    .etudiant {
+        left: 5px;
+    }
+    .etudiant #nom {
+        font-size: 6px;
+    }
+    .etudiant #photoProfil {
+        margin: 0;
+    }
+}
+@media screen and (min-width: 0px) and (max-width: 339px) {
+    #button a{
+        font-size: 5px;
+        padding: 0 5px;
+    }
+    #buttons a{
+        font-size: 5px;
+        padding: 0 5px;
+    }
+    #button img{
+        width: 10px;
+    }
+    #buttons img{
+        width: 10px;
+    }
+    #desktop {
+        display: none;
+    }
+    #mobile{
+        display: initial;
+    }
+    #buttons{
+        height: 26px;
+    }
+    .service{
+        font-size: 8px;
+    }
+    .categorie{
+        font-size: 8px
+    }
+    #copyright{
+        font-size: 3px;
+        color: #ffffff;
+        opacity: 50%;
+        text-align: center;
+        position: absolute;
+        bottom: 15px;
+        left: 2px;
+    }
+    .etudiant {
+        left: 5px;
+    }
+    .etudiant #nom {
+        font-size: 5px;
+    }
+    .etudiant #photoProfil {
+        margin: 0;
+    }
 }
 </style>

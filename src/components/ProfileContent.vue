359cd78b7e80da5bc8f7ed445f671969cd224db1
ekypow <lucas.schrever@gmail.com>
2020-05-12 09:38:20 +0200

    composant w/ css no responsive

--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -67,5 +67,162 @@
 </script>
 
 <style scoped>
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed,
+    figure, figcaption, footer, header, hgroup,
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+    }
+    /* HTML5 display-role reset for older browsers */
+    article, aside, details, figcaption, figure,
+    footer, header, hgroup, menu, nav, section {
+        display: block;
+    }
+    body {
+        line-height: 1;
+    }
+    ol, ul {
+        list-style: none;
+    }
+    blockquote, q {
+        quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+
+
+
+
+
+
+    body {
+        font-family: Montserrat, sans-serif;
+        background-color: #121212;  /*fond noir*/
+        color: #FFFFFF;
+    }
+
+    #account {
+        float: left;
+        color: rgba(255,255,255,0.51)
+    }
+
+    .lang {
+        float: right;
+        font-weight: bold;
+        font-size: 17px;
+    }
+
+    .top {
+        margin-bottom: 10vh;
+        padding: 4vh 1vw;
+        border: 1px solid green;
+    }
+
+    .content {         /*base du grid pour les infos*/
+        display: grid;
+        grid-template-columns: 10% 18% 25% 29% 13%;
+        column-gap: 40px;
+        border: 1px solid red;
+    }
+
+    .info1 {
+        grid-column: 2/3;
+        text-align: center;
+        border: 1px solid white;
+    }
+
+    .pp {
+        border: 2px solid #FF8800;
+        border-radius: 50%;
+    }
+
+    h3 {
+        margin: 50px 0;
+    }
+
+    .modif {
+        background-color: #242424;
+        padding: 20px 5px;
+        border: 2px solid #FF8800;
+        border-radius: 4px;
+        font-family: Montserrat, sans-serif;
+        font-weight: bold;
+        font-size: 16px;
+        color: #FFFFFF;
+    }
+
+    .info2 {
+        grid-column: 3/4;
+        border: 1px solid white;
+    }
+
+
+    .info3 {
+        grid-column: 4/5;
+        border: 1px solid white;
+    }
+
+    input {
+        width: 85%;
+        margin: 5px 0 35px 0;
+        padding: 7px 5px;
+        background-color: #242424;
+        border-style: none;
+        border-radius: 4px;
+        font-family: Montserrat, sans-serif;
+        color: #FFFFFF;
+    }
+
+    textarea {
+        width: 85%;
+        margin: 5px 0 35px 0;
+        padding: 7px 5px;
+        background-color: #242424;
+        border-style: none;
+        border-radius: 4px;
+        font-family: Montserrat, sans-serif;
+        color: #FFFFFF;
+    }
+
+
+    h3, #account, .info2 label, .info3 label {
+        font-weight: bold;
+        font-size: 20px;
+    }
+
+    /*Bouton qui s'affiche en fonction de taille de l'écran*/
+    .modif2 {
+        background-color: #242424;
+        padding: 20px 5px;
+        border: 2px solid #FF8800;
+        border-radius: 4px;
+        font-family: Montserrat, sans-serif;
+        font-weight: bold;
+        font-size: 16px;
+        color: #FFFFFF;
+        display: none;
+    }
 
 </style>

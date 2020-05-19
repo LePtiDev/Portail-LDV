38b01df26466600822004ae1c734773ef7b7af9a
Quentin <75quentinguerrier@gmail.com>
2020-05-19 12:48:20 +0200

    add component check button

---
+++ src/components/CheckButton.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="bouton">
+        <p>JE SUIS PRÉSENT</p>
+        <div class=timer>
+            <img src="../assets/horloge.png" alt="">
+            <p class="time">4:39</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CheckButton',
+    props: {
+        student: String,
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+.bouton{
+    height: 94px;
+    width: 100%;
+    background-color: #27AE60;
+    border-radius: 4px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px;
+}
+
+.timer{
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    align-items: center;
+}
+
+.time{
+    margin-left: 5px;
+}
+
+</style>
--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,24 +1,39 @@
 <template>
-  <div class="container">
-    <h2>TABLEAU DE BORD</h2>
-    <div class="grid">
-      <div class="presence">
-        <p class="section-title">RELEVÉ DE PRÉSENCE</p>
-        <p class="section-underTitle">Devlab S2 - 10/03</p>
-        <div class=""></div>
-      </div>
-      <div class="information"></div>
-      <div class="edt"></div>
-      <div class="absence"></div>
-      <div class="mark"></div>
-      <div class="contact"></div>
+    <div class="container">
+        <h2>TABLEAU DE BORD</h2>
+        <div class="grid">
+            <div class="rows row-one">
+                <div class="section presence">
+                    <p class="section-title">RELEVÉ DE PRÉSENCE</p>
+                    <p class="section-underTitle">Devlab S2 - 10/03</p>
+                    <CheckButton/>
+                </div>
+                <div class="space"></div>
+                <div class="section information"></div>
+            </div>
+            <div class="rows row-two">
+                <div class="section edt"></div>
+                <div class="space"></div>
+                <div class="section absence"></div>
+                <div class="space"></div>
+                <div class="section mark"></div>
+            </div>
+            <div class="rows row-three">
+                <div class="section contact"></div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
+
+import CheckButton from '../components/CheckButton'
+
 export default {
   name: "Dashboard",
+  components: {
+      CheckButton,
+  },
   props: {
     msg: String
   }
@@ -47,67 +62,88 @@ h2 {
   filter: opacity(0.5);
 }
 
-/* Grille */
-
-.grid {
-  display: grid;
-  grid-template-columns: 28% 28% 40%;
-  grid-gap: 2vh;
-  grid-template-rows: 212px 400px 212px;
-  margin: 40px 0px 40px 0px;
-}
-
-/* sections */
-
-.presence{
-  grid-column: 1;
-  grid-row: 1;
-  background-color: #242424;
-  padding: 30px;
-}
-
-.information{
-  grid-column: 2/4;
-  grid-row: 1;
-  background-color: #242424;
-}
-
-.edt{
-  grid-column:1;
-  grid-row: 2;
-  background-color: #242424;
-}
-
-.absence{
-  grid-column:2;
-  grid-row: 2;
-  background-color: #242424;
-}
-
-.mark{
-  grid-column:3;
-  grid-row: 2;
-  background-color: #242424;
-}
-
-.contact {
-  grid-column:1/4;
-  grid-row: 3;
-  background-color: #242424;
-}
-
-/* section presence */
-
 .section-title{
   font-family: 'Montserrat';
   font-weight: 600;
   margin-bottom: 5px;
-  font-size: 20px;
+  font-size: 18px;
 }
 
 .section-underTitle{
-  font-size: 20px;
+  font-size: 18px;
 }
+
+/* Grille */
+
+.grid {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 40px 0px 40px 0px;
+  width: 100%;
+  height: auto;
+}
+
+/* rows */
+.rows{
+    width: 100%;
+    margin: 0 0 2vw 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.row-one{
+    height: 212px;
+}
+
+.row-two{
+    height: 500px;
+}
+
+.row-three{
+    height: 212px;
+}
+
+/* sections */
+.section{
+    padding: 30px;
+    background-color: #242424;
+    height: 100%;
+    border-radius: 4px;
+}
+
+.space{
+    height: 100%;
+    width: 2vw;
+}
+
+.presence{
+    width: 30%;
+}
+
+.information{
+    width: 68%;
+}
+
+.edt{
+    width: 30%;
+}
+
+.absence{
+    width: 28%;
+}
+
+.mark{
+    width: 38%;
+}
+
+.contact {
+    width: 100%;
+}
+
+/* section presence */
 
 .button-presence{
   height: 100px;
